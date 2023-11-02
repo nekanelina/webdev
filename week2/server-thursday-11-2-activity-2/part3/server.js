b7e4692853f5973be365ca2daa062d3ee6d09561
Augustas <augis.reipsleger@gmail.com>
2023-11-02 12:09:42 +0200

    part 3 middleware

---
+++ week2/server-thursday-11-2-activity-2/part3/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+// Define a custom middleware function for logging
+const logMiddleware = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  next(); // Continue to the next middleware or route handler
+};
+
+// Use the logging middleware for all routes
+app.use(logMiddleware);
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express with Logging!');
+});
+
+app.get('/text', (req, res) => {
+  res.send('Hello, Express with Logging! TEXT');
+  console.log('Hello, Express with Logging! TEXT');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
